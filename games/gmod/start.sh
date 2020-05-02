e01eca68c978b04e719df96fda7d8a79eeb5e7e0
Sean Boult <hacksore@mcskinsearch.com>
2020-05-02 16:09:56 -0500

    Add WIP code

--- games/gmod/start.sh
+++ games/gmod/start.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# was debugging microk8s
-echo "Checking if we have internet"
-curl -s 'https://api.ipify.org?format=json'
-
-# Tell sidecar everything is artificially "ok" until I write something better
-# to check that the server is actually up by reading the console output
-for i in {1..3}; do
-  curl -s -d "{}" -H "Content-Type: application/json" -X POST http://localhost:${AGONES_SDK_HTTP_PORT}/ready
-  sleep 1
-done
-
 ${STEAMAPPDIR}/srcds_run \
   -game garrysmod \
   -maxplayers $SRCDS_MAXPLAYERS \
