ae2dbba46018e0a8f493f39b4273bfe1c91976f0
Sean Boult <hacksore@mcskinsearch.com>
2020-05-05 03:17:28 +0000

    Allow port to be sourced from ENV

--- games/gmod/start.sh
+++ games/gmod/start.sh
@@ -4,8 +4,11 @@
 echo "Checking if we have internet"
 curl -s 'https://api.ipify.org?format=json'
 
+echo "Starting game server..."
+
 ${STEAMAPPDIR}/srcds_run \
   -game garrysmod \
+  -port $DSG_PORT \
   -maxplayers $SRCDS_MAXPLAYERS \
   +map $SRCDS_STARTMAP \
   +sv_setsteamaccount $SRCDS_TOKEN \
